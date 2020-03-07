2a2edd0df78d49ec421cf11c024c5b2e1d0661ed
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-07 21:31:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,9 +53,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by
-    sql="SELECT * FROM #{self.table_name} WHERE id = ?"
-    DB[:conn].execute(sql, id)
+  def self.find_by(grade)
+    sql="SELECT * FROM #{self.table_name} WHERE grade = ?"
+    DB[:conn].execute(sql, grade)
   end
 
 end
